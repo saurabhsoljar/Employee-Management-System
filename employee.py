bebda3c5f9ddb1436176c88f78c402cf6b325663
saurabh kumar <saurabhslite2@gmail.com>
2025-03-07 14:40:52 +0530

    create Employee Information fill section or we can say upper Frame design complete

--- employee.py
+++ employee.py
@@ -1,80 +1,292 @@
-from tkinter import*
+from tkinter import *
 from tkinter import ttk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
+
 
 class Employee:
-    def __init__(self,root):
-        self.root=root
+    def __init__(self, root):
+        self.root = root
         self.root.geometry("1530x790+0+0")
-        self.root.title('Employee Management System')
-        
+        self.root.title("Employee Management System")
+
         lbl_title = Label(
-          self.root,
-          text='EMPLOYEE MANAGEMENT SYSTEM',
-          font=('times new roman', 37, 'bold'),   
-          fg='darkblue',
-          bg='white'
+            self.root,
+            text="EMPLOYEE MANAGEMENT SYSTEM",
+            font=("times new roman", 37, "bold"),
+            fg="darkblue",
+            bg="white",
         )
         lbl_title.place(x=0, y=0, relwidth=1.0, height=50)
-        
-        #Image Frame
-        img_logo=Image.open('images/logo-1.jpg')
+
+        # Image Frame
+        img_logo = Image.open("images/logo-1.jpg")
         img_logo = img_logo.resize((50, 50), Image.LANCZOS)
-        self.photo_logo=ImageTk.PhotoImage(img_logo)
-        
-        self.logo=Label(self.root,image=self.photo_logo)
-        
+        self.photo_logo = ImageTk.PhotoImage(img_logo)
+
+        self.logo = Label(self.root, image=self.photo_logo)
+
         left_margin = 250
         self.logo.place(x=20 + left_margin, y=0, width=50, height=50)
-        
-        img_frame=Frame(self.root,bd=2,relief=RIDGE,bg='white')
-        img_frame.place(x=0,y=50,width=1530,height=160)
-        
+
+        img_frame = Frame(self.root, bd=2, relief=RIDGE, bg="white")
+        img_frame.place(x=0, y=50, width=1530, height=160)
+
         # 1st Logo
-        img1 = Image.open('images/p1.png')
-        img1 = img1.resize((540, 160), Image.LANCZOS) 
-        self.photo1 = ImageTk.PhotoImage(img1) 
+        img1 = Image.open("images/p1.png")
+        img1 = img1.resize((540, 160), Image.LANCZOS)
+        self.photo1 = ImageTk.PhotoImage(img1)
 
         self.img_1 = Label(img_frame, image=self.photo1)
         self.img_1.place(x=0, y=0, width=540, height=160)
-        
-        
+
         # 2st Logo
-        img2 = Image.open('images/p2.png')
-        img2 = img2.resize((540, 160), Image.LANCZOS) 
-        self.photo2 = ImageTk.PhotoImage(img2) 
+        img2 = Image.open("images/p2.png")
+        img2 = img2.resize((540, 160), Image.LANCZOS)
+        self.photo2 = ImageTk.PhotoImage(img2)
 
         self.img_2 = Label(img_frame, image=self.photo2)
         self.img_2.place(x=540, y=0, width=540, height=160)
-        
-      
+
         # 3st Logo
-        img3 = Image.open('images/p3.png')
-        img3 = img3.resize((540, 160), Image.LANCZOS) 
-        self.photo3 = ImageTk.PhotoImage(img3) 
+        img3 = Image.open("images/p3.png")
+        img3 = img3.resize((540, 160), Image.LANCZOS)
+        self.photo3 = ImageTk.PhotoImage(img3)
 
         self.img_3 = Label(img_frame, image=self.photo1)
         self.img_3.place(x=1000, y=0, width=540, height=160)
-        
+
         # main Frame
-        Main_frame = Frame(self.root, bd=2, relief=RIDGE, bg='white')
+        Main_frame = Frame(self.root, bd=2, relief=RIDGE, bg="white")
         Main_frame.place(x=10, y=215, width=1500, height=560)
 
         # upper Frame
-        upper_frame = LabelFrame(Main_frame, relief=RIDGE, bg='white', text='Employee Information', font=('times new roman', 11, 'bold'), fg='red')
+        upper_frame = LabelFrame(
+            Main_frame,
+            relief=RIDGE,
+            bg="white",
+            text="Employee Information",
+            font=("times new roman", 11, "bold"),
+            fg="red",
+        )
         upper_frame.place(x=10, y=10, width=1480, height=270)
-        
+
+        # Labels and Enter fields
+        lbl_dep = Label(
+            upper_frame, text="Department", font=("arial", 11, "bold"), bg="white"
+        )
+        lbl_dep.grid(row=0, column=0, padx=2, sticky=W)
+
+        combo_dep = ttk.Combobox(
+            upper_frame, font=("arial", 12, "bold"), width=17, state="readonly"
+        )
+        combo_dep["value"] = (
+            "Select Department",
+            "HR",
+            "Software Engineer",
+            "Manager",
+            "Sales",
+            "Marketing",
+            "Finance",
+            "Data Science",
+            "Product Management",
+            "Customer Support",
+            "Quality Assurance",
+        )
+        combo_dep.current(0)
+        combo_dep.grid(row=0, column=1, padx=2, pady=10, sticky=W)
+
+        # name
+        lbl_name = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Name:", bg="white"
+        )
+        lbl_name.grid(row=0, column=2, sticky=W, padx=2, pady=7)
+
+        txt_name = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_name.grid(row=0, column=3, padx=2, pady=7)
+
+        # lbl_Designition
+        lbl_Designition = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Designition:", bg="white"
+        )
+        lbl_Designition.grid(row=1, column=0, sticky=W, padx=2, pady=7)
+
+        txt_Designition = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_Designition.grid(row=1, column=1, sticky=W, padx=2, pady=7)
+
+        # Email
+        lbl_email = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Email:", bg="white"
+        )
+        lbl_email.grid(row=1, column=2, sticky=W, padx=2, pady=2)
+
+        txt_email = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_email.grid(row=1, column=3, padx=2, pady=7)
+
+        # Address
+        lbl_adderss = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Address:", bg="white"
+        )
+        lbl_adderss.grid(row=2, column=0, sticky=W, padx=2, pady=7)
+
+        txt_adderss = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_adderss.grid(row=2, column=1, padx=2, pady=7)
+
+        # married
+        lbl_merried_status = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Married Status:", bg="white"
+        )
+        lbl_merried_status.grid(row=2, column=2, sticky=W, padx=2, pady=7)
+
+        com_txt_merried = ttk.Combobox(
+            upper_frame, state="readonly", width=20, font=("arial", 11, "bold")
+        )
+        com_txt_merried["value"] = ("Merried", "Unmarried")
+        com_txt_merried.current(0)
+        com_txt_merried.grid(row=2, column=3, sticky=W, padx=2, pady=7)
+
+        # Dob
+        lbl_dob = Label(
+            upper_frame, font=("arial", 12, "bold"), text="DOB:", bg="white"
+        )
+        lbl_dob.grid(row=3, column=0, sticky=W, padx=2, pady=7)
+
+        txt_dob = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_dob.grid(row=3, column=1, padx=2, pady=7)
+
+        # Doj
+        lbl_doj = Label(
+            upper_frame, font=("arial", 12, "bold"), text="DOJ:", bg="white"
+        )
+        lbl_doj.grid(row=3, column=2, sticky=W, padx=2, pady=7)
+
+        txt_doj = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_doj.grid(row=3, column=3, padx=2, pady=7)
+
+        # Id Proof
+        com_txt_proof = ttk.Combobox(
+            upper_frame, state="readonly", font=("arial", 12, "bold"), width=22
+        )
+        com_txt_proof["values"] = (
+            "Select ID Proof",
+            "PAN CARD",
+            "ADHAR CARD",
+            "DRIVING LICENSE",
+            "PASSPORT",
+        )
+        com_txt_proof.current(0)
+        com_txt_proof.grid(row=4, column=0, sticky=W, padx=2, pady=7)
+
+        txt_proof = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_proof.grid(row=4, column=1, padx=2, pady=7)
+
+        # gender
+        lbl_gender = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Gender:", bg="white"
+        )
+        lbl_gender.grid(row=4, column=2, sticky=W, padx=2, pady=7)
+
+        com_txt_gender = ttk.Combobox(
+            upper_frame, state="readonly", font=("arial", 12, "bold"), width=18
+        )
+
+        com_txt_gender["values"] = ("Male", "Female", "Other")
+        com_txt_gender.current(0)
+        com_txt_gender.grid(row=4, column=3, sticky=W, padx=2, pady=7)
+
+        # phone
+        lbl_phone = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Phone No:", bg="white"
+        )
+        lbl_phone.grid(row=0, column=4, sticky=W, padx=2, pady=7)
+
+        txt_phone = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_phone.grid(row=0, column=5, padx=2, pady=7)
+
+        # country
+        lbl_country = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Country:", bg="white"
+        )
+        lbl_country.grid(row=1, column=4, sticky=W, padx=2, pady=7)
+
+        txt_country = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_country.grid(row=1, column=5, padx=2, pady=7)
+
+        # CTC
+        lbl_ctc = Label(
+            upper_frame, font=("arial", 12, "bold"), text="Salary(CTC):", bg="white"
+        )
+        lbl_ctc.grid(row=2, column=4, sticky=W, padx=2, pady=7)
+
+        txt_ctc = ttk.Entry(upper_frame, width=22, font=("arial", 11, "bold"))
+        txt_ctc.grid(row=2, column=5, padx=2, pady=7)
+
+        # Profile img
+        img_mask = Image.open("images/saurabh.jpg")
+        img_mask = img_mask.resize((200, 220), Image.LANCZOS)
+        self.photomask = ImageTk.PhotoImage(img_mask)
+
+        self.img_mask = Label(
+            upper_frame, image=self.photomask
+        )  # Corrected lebal to Label
+        self.img_mask.place(x=1025, y=0, width=200, height=220)
+
+        # Button Frame
+        button_frame = Frame(upper_frame, bd=2, relief=RIDGE, bg="white")
+        button_frame.place(x=1290, y=10, width=170, height=210)
+
+        btn_add = Button(
+            button_frame,
+            text="Save",
+            font=("arial", 15, "bold"),
+            width=13,
+            bg="blue",
+            fg="white",
+        )
+        btn_add.grid(row=0, column=0, padx=1, pady=5)
+
+        btn_update = Button(
+            button_frame,
+            text="Update",
+            font=("arial", 15, "bold"),
+            width=13,
+            bg="blue",
+            fg="white",
+        )
+        btn_update.grid(row=1, column=0, padx=1, pady=5)
+
+        btn_delete = Button(
+            button_frame,
+            text="Delete",
+            font=("arial", 15, "bold"),
+            width=13,
+            bg="blue",
+            fg="white",
+        )
+        btn_delete.grid(row=2, column=0, padx=1, pady=5)
+
+        btn_clear = Button(
+            button_frame,
+            text="Clear",
+            font=("arial", 15, "bold"),
+            width=13,
+            bg="blue",
+            fg="white",
+        )
+        btn_clear.grid(row=3, column=0, padx=1, pady=5)
+
         # down Frame
-        down_frame = LabelFrame(Main_frame, relief=RIDGE, bg='white', text='Employee Information Table', font=('times new roman', 11, 'bold'), fg='red')
+        down_frame = LabelFrame(
+            Main_frame,
+            relief=RIDGE,
+            bg="white",
+            text="Employee Information Table",
+            font=("times new roman", 11, "bold"),
+            fg="red",
+        )
         down_frame.place(x=10, y=280, width=1480, height=270)
-        
-        
-        
-        
-        
-        
-        
-if __name__=="__main__":
-    root=Tk()
-    obj=Employee(root)
-    root.mainloop()        
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Employee(root)
+    root.mainloop()
